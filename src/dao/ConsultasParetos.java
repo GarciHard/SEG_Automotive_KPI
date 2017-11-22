f9312ce0ddce5f70d50f6033f91a5f50455bd532
arelipca <areli.calixto@gmail.com>
2017-11-22 11:29:54 -0600

    QUERYS en java

--- src/dao/ConsultasParetos.java
+++ src/dao/ConsultasParetos.java
@@ -1,13 +1,24 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import modelo.CalidadDAO;
 import modelo.ConexionBD;
+import vista.Reportes;
 
 /**
  *
  * @author PRR1TL
  */
 public class ConsultasParetos extends ConexionBD {
+    private ArrayList lisPzasAnio;
+    private ArrayList lisPzasMes;
+    private ArrayList lisPzasDia;
+    private ArrayList lisPzasDesglos;
+    
+    private PreparedStatement ps;
+    private ResultSet rs;
     
     //CONSULTAS PARA PIEZAS PRODUCIDAS
     private final String PZAS_ANIO = "SELECT Anio, SUM(cantPzas) FROM Bitacora WHERE linea LIKE ?  AND anio LIKE ? GROUP BY anio";
@@ -27,7 +38,33 @@ public class ConsultasParetos extends ConexionBD {
     private final String ORG_DIA = "SELECT dia, SUM (duracion) FROM Bitacora WHERE tema = 'Tecnizas' AND linea LIKE ? AND anio LIKE ? AND mes LIKE ? GROUP BY dia";
     private final String ORG_DESGLOS = "SELECT area, problema, SUM (duracion) FROM Bitacora WHERE tema = 'Tecnizas' AND LIKE ? AND anio LIKE ? AND mes LIKE ? GROUP BY problema";
     
-    
-    
+    public ArrayList conPzasAnio (String linea, String anio) throws Exception{
+        Object[] bitacoraObj;
+        lisPzasAnio = new ArrayList();  
+        try {
+            this.conectar();
+            ps = this.conexion.prepareStatement(PZAS_ANIO);
+            ps.setString(1, linea);
+            ps.setString(2, anio);
+            
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                bitacoraObj = new Object[3];
+                bitacoraObj[0] = rs.getString(1);
+                bitacoraObj[1] = rs.getString(2);
+                
+                lisPzasAnio.add(bitacoraObj);
+            }
+            System.out.println(lisPzasAnio);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            ps.close();
+            rs.close();
+            this.cerrar();
+        }
+        return lisPzasAnio;
+    }    
     
 }
