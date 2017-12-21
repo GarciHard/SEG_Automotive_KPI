25064ce0d11d1e2289bda56335e5f8549c08a9a7
PRR1TL <PRR1TL@TL12BA01.sg.lan>
2017-12-21 15:44:30 -0600

    Targets

--- src/dao/CargaTargetEntregas.java
+++ src/dao/CargaTargetEntregas.java
@@ -11,23 +11,30 @@ import modelo.ConexionBD;
 public class CargaTargetEntregas extends ConexionBD {
     private PreparedStatement ps;
     private ResultSet rs;
+    //metaTec, metaOrg, metaCalidad, metaScrap
+    private final String INSERTAR_DIA_MES_ANIO = "INSERT INTO targets(linea, anio, mes, dia, tek, producidas, tecnicas, organizacionales, calidad, scrap, cambio, planeado) " + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    //private final String INSERTAR_TEK = "UPDATE targets SET tek = ? WHERE linea LIKE ? AND anio LIKE ? AND mes LIKE ? and dia LIKE ?";
+    //private final String INSERTAR_PROGRAMADO = "UPDATE targets SET programado = ? WHERE linea LIKE ? AND anio LIKE ? AND mes LIKE ? and dia LIKE ?";
+    private final String CONSULTA_FECHA_REGISTRO = "SELECT COUNT (dia) FROM targets WHERE linea LIKE ? AND anio LIKE ? AND mes LIKE ? and dia LIKE ?";
     
-    private final String INSERTAR_DIA_MES_ANIO = "INSERT INTO targetEntregas(linea, anio, mes, noMes, dia, tek, programado) " + "VALUES(?, ?, ?, ?, ?, ? , ?)";
-    private final String INSERTAR_TEK = "UPDATE targetEntregas SET tek = ? WHERE linea LIKE ? AND anio LIKE ? AND mes LIKE ? and noMes LIKE ? and dia LIKE ?";
-    private final String INSERTAR_PROGRAMADO = "UPDATE targetEntregas SET programado = ? WHERE linea LIKE ? AND anio LIKE ? AND mes LIKE ? and noMes LIKE ? and dia LIKE ?";
-    private final String CONSULTA_FECHA_REGISTRO = "SELECT COUNT (dia) FROM targetEntregas WHERE linea LIKE ? AND anio LIKE ? AND mes LIKE ? and noMes LIKE ? and dia LIKE ?";
-    
-    public void insertarFechaYLinea(String linea, String anio, String mes, String noMes, String dia, String trek, String programado) throws Exception {
+    public void insertarFechaYLinea(String linea, String anio, String mes, String dia, String trek, String programado, String tecnicas, String organizacionales, String calidad, String scrap, String cambios, String planeados) throws Exception {
         try {
             this.conectar();            
             ps = this.conexion.prepareStatement(INSERTAR_DIA_MES_ANIO);
             ps.setString(1, linea);//linea)
             ps.setString(2, anio);//anio
             ps.setString(3, mes);//mes
-            ps.setString(4, noMes); 
-            ps.setString(5, dia);//dia
-            ps.setString(6, trek); 
-            ps.setString(7, programado);//dia
+            ps.setString(4, dia);//dia
+            ps.setString(5, trek); 
+            ps.setString(6, programado);
+            ps.setString(7, tecnicas); 
+            ps.setString(8, organizacionales);
+            ps.setString(9, calidad); 
+            ps.setString(10, scrap);
+            ps.setString(11, cambios); 
+            ps.setString(12, planeados);
+            
+            //System.out.println(linea+anio+mes+dia+trek+programado+tecnicas+organizacionales+calidad+scrap+"\n");
 
             ps.executeUpdate();
         } catch (Exception e) {
@@ -39,44 +46,43 @@ public class CargaTargetEntregas extends ConexionBD {
         }
     }   
     
-    public void insertarTEK(String tek, String linea, String anio, String mes, String noMes, String dia) throws Exception {
-        try {
-            this.conectar();     
-            ps = this.conexion.prepareStatement("UPDATE targetEntregas SET tek = ? WHERE linea='"+ linea +"' and anio='"+ anio +"' and mes='"+ mes + "' and dia='" + dia + "'");
-            //ps = this.conexion.prepareStatement(INSERTAR_TEK);
-            ps.setString(1, tek);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.err.println("update" + e);
-            throw e;
-        } finally {
-            ps.close();
-            this.cerrar();
-        }
-    }   
+//    public void insertarTEK(String tek, String linea, String anio, String mes, String noMes, String dia) throws Exception {
+//        try {
+//            this.conectar();     
+//            ps = this.conexion.prepareStatement("UPDATE targetEntregas SET tek = ? WHERE linea='"+ linea +"' and anio='"+ anio +"' and mes='"+ mes + "' and dia='" + dia + "'");
+//            //ps = this.conexion.prepareStatement(INSERTAR_TEK);
+//            ps.setString(1, tek);
+//            ps.executeUpdate();
+//        } catch (Exception e) {
+//            System.err.println("update" + e);
+//            throw e;
+//        } finally {
+//            ps.close();
+//            this.cerrar();
+//        }
+//    }   
     
-    public void insertarProgramado(String programado, String linea, String anio, String mes, String noMes, String dia) throws Exception {
-        try {
-            this.conectar();            
-            ps = this.conexion.prepareStatement(INSERTAR_PROGRAMADO);
-            ps.setString(1, programado);
-            ps.setString(2, linea);
-            ps.setString(3, anio);
-            ps.setString(4, mes);
-            ps.setString(5, noMes);
-            ps.setString(6, dia);
-            
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.err.println("Error en INSERT " + e);
-            throw e;
-        } finally {
-            ps.close();
-            this.cerrar();
-        }
-    }   
+//    public void insertarProgramado(String programado, String linea, String anio, String mes, String dia) throws Exception {
+//        try {
+//            this.conectar();            
+//            ps = this.conexion.prepareStatement(INSERTAR_PROGRAMADO);
+//            ps.setString(1, programado);
+//            ps.setString(2, linea);
+//            ps.setString(3, anio);
+//            ps.setString(4, mes);
+//            ps.setString(5, dia);
+//            
+//            ps.executeUpdate();
+//        } catch (Exception e) {
+//            System.err.println("Error en INSERT " + e);
+//            throw e;
+//        } finally {
+//            ps.close();
+//            this.cerrar();
+//        }
+//    }   
     
-    public int consultaFechaExistente(String linea, String anio, String mes, String noMes, String dia) throws Exception{
+    public int consultaFechaExistente(String linea, String anio, String mes, String dia) throws Exception{
         int bn = 0;
         try {   
             this.conectar();
@@ -85,8 +91,7 @@ public class CargaTargetEntregas extends ConexionBD {
             ps.setString(1, linea);
             ps.setString(2, anio);
             ps.setString(3, mes);
-            ps.setString(4, noMes);
-            ps.setString(5, dia);
+            ps.setString(4, dia);
             
             rs = ps.executeQuery();  
             if (rs.next()){  
